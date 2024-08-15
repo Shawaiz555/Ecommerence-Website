fb3820d5019bad5ef9a237f17123c83cc9ffc2d8
Shawaiz555 <shawaizbutt555@gmail.com>
2024-08-15 16:56:07 +0500

    added the contact form

---
+++ Contact.js
@@ -0,0 +1,93 @@
+let LoginedUserName = document.querySelector('#LoginedUserName');
+const Username = localStorage.getItem('LoggedInUser');
+if(Username == null)
+{
+    LoginedUserName.innerHTML = `
+        <a href="#"
+                    ><img
+                      class="shoppingicon"
+                      src="./shoppingicon.png"
+                      alt="Shopping icon"
+                      width="30"
+                      height="30"
+                  /></a>
+    `
+}
+else
+{
+    LoginedUserName.innerHTML = Username;
+    let LogOutbtn = document.querySelector('#LogOutbtn');
+        LogOutbtn.innerHTML = `
+            <button class="LogOutbtn">LOG OUT</button>
+        `;
+        let Signuplink = document.getElementById('Signuplink');
+        let Loginlink = document.getElementById('Loginlink');
+        Signuplink.style.display = 'none';
+        Loginlink.style.display = 'none';
+}
+
+const LogOutbtn = document.getElementById('LogOutbtn');
+LogOutbtn.onclick = function()
+{
+    localStorage.removeItem('LoggedInUser');
+    alert('Logged Out Successfully');
+    let Signuplink = document.getElementById('Signuplink');
+    let Loginlink = document.getElementById('Loginlink');
+    Signuplink.style.display = 'block';
+    Loginlink.style.display = 'block';
+    let LoginedUserName = document.querySelector('#LoginedUserName');
+    LoginedUserName.innerHTML = `
+        <a href="#"
+                    ><img
+                      class="shoppingicon"
+                      src="./shoppingicon.png"
+                      alt="Shopping icon"
+                      width="30"
+                      height="30"
+                  /></a>
+    `;
+    let LogOutbtn = document.querySelector('#LogOutbtn');
+        LogOutbtn.innerHTML = `
+            <a href="#"
+                    ><img
+                      class="contacticon"
+                      src="./contacticon.png"
+                      alt="Contact icon"
+                      width="30"
+                      height="30"
+                  /></a>
+        `;
+}
+
+
+
+let Sendbtn = document.getElementById('Sendbtn');
+
+Sendbtn.onclick = function()
+{
+const ContactInfo = JSON.parse(localStorage.getItem('ContactInfo')) || [];
+
+let Fullname = document.getElementById('Fullname').value;
+let Phonenumber = document.getElementById('Phonenumber').value;
+let Email = document.getElementById('Email').value;
+let Address = document.getElementById('Address').value;
+if(Fullname!="" && Phonenumber!="" && Email!="" && Address!="")
+{
+    let Contact = {
+        Fullname: Fullname,
+        Phonenumber: Phonenumber,
+        Email: Email,
+        Address: Address
+    }
+    ContactInfo.push(Contact);
+    console.log(ContactInfo);
+    localStorage.setItem('ContactInfo', JSON.stringify(ContactInfo));
+    alert(`Hey ${Fullname}, Your Information is sent Successfully!!!`);
+}
+else
+{
+    alert('Enter the Full Information First Sir!!');
+}
+}
+
+
